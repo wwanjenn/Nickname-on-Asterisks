936b1bc491dabdaee711457d96ec0d857fef30a4
Wanjin Maglangit <wmaglangit1118@gmail.com>
2022-10-22 12:08:29 +0800

    Finished code, but no nested list yet, tried testing it with print and it works

--- Assignment no.1.py
+++ Assignment no.1.py
@@ -2,10 +2,16 @@
 # Create a program that will print your nickname using only asterisk character (*)
 
 # J
+
 for row in range(8):
     for col in range (10):
-        if row == 0 or row == 1:
-            print("*")
-        if col == 4 or col == 5:
-            print("*")
-        else: print(" ")
+        if (row == 0 or row == 1) and (col != 0 or col != 9):
+            print("*", end = "")
+        elif (row != 0 or row != 1) and (col == 4 or col == 5):
+            print("*", end = "")
+        elif (row == 4 or row == 5 or row == 6 or row == 7) and (col == 1 or col == 2):
+            print("*", end = "")
+        elif (row == 6 or row == 7) and col == 3:
+            print("*", end = "")
+        else: print(end = " ")
+    print()
